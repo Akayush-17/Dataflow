e09e5392fa5703c9cd06bd29dc697f3ae24488f4
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-05-15 00:45:42 +0530

    first commit

---
+++ src/components/certificate.js
@@ -0,0 +1,12 @@
+import cimage from "../assets/xyz.png"
+
+function Certificate(){
+    return (
+        <div className="Container">
+            <img src={cimage} className="img"  alt="cimage"></img>
+        </div>
+
+    )
+}
+
+export default Certificate
